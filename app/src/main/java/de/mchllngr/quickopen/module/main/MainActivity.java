c0d09aeaa45dae37b38e06627def307383663373
Michael Langer <dev@mlanger.net>
2016-09-22 01:18:53 +0200

    styled snackbar for better visibility in night-mode

--- app/src/main/java/de/mchllngr/quickopen/module/main/MainActivity.java
+++ app/src/main/java/de/mchllngr/quickopen/module/main/MainActivity.java
@@ -1,6 +1,7 @@
 package de.mchllngr.quickopen.module.main;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -270,6 +271,7 @@ public class MainActivity extends BaseActivity<MainView, MainPresenter>
                         snackbar.dismiss();
                     }
                 });
+        snackbar.getView().setBackgroundResource(R.color.snackbar_background_color);
 
         snackbar.show();
     }
