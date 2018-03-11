fb45fa0d5b5f805c2e4f942c5f67d5059dd99bb5
Michael Langer <dev@mlanger.net>
2018-03-11 23:52:59 +0100

    removed settings, send to android-notifications-settings instead;

--- app/src/main/java/de/mchllngr/quickopen/service/NotificationService.java
+++ app/src/main/java/de/mchllngr/quickopen/service/NotificationService.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NotificationCompat;
-import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.f2prateek.rx.preferences.Preference;
@@ -21,23 +19,10 @@ import de.mchllngr.quickopen.model.ApplicationModel;
 import de.mchllngr.quickopen.util.CustomNotificationHelper;
 import de.mchllngr.quickopen.util.GsonPreferenceAdapter;
 
-import static android.os.Build.VERSION;
-import static android.os.Build.VERSION_CODES;
-
 /**
  * Foreground-{@link Service} for handling the notification
  */
 public class NotificationService extends Service {
-
-    private static final int NOTIFICATION_ICON_ID_NOT_VECTOR = R.drawable.ic_notification;
-    private static final int NOTIFICATION_ICON_ID_VECTOR = R.drawable.ic_speaker_notes_white_24px;
-    private static final int NOTIFICATION_ICON_ID_NOT_VECTOR_TRANSPARENT = R.drawable.ic_notification_blank;
-    private static final int NOTIFICATION_ICON_ID_VECTOR_TRANSPARENT = R.drawable.ic_blank_24px;
-
-    /**
-     * Allows usage of VectorDrawables when current {@link VERSION} is Android Lollipop or newer.
-     */
-    private static final boolean USE_VECTOR_DRAWABLES = VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP;
 
     /**
      * Determines if the notification is enabled and should be shown.
@@ -48,25 +33,10 @@ public class NotificationService extends Service {
      */
     private CustomNotificationHelper customNotificationHelper;
     /**
-     * {@link Preference}-reference for easier usage of the saved value for transparentIcon
-     * in the {@link RxSharedPreferences}.
-     */
-    private Preference<Boolean> transparentIconPref;
-    /**
      * {@link Preference}-reference for easier usage of the saved value for notificationEnabled
      * in the {@link RxSharedPreferences}.
      */
     private Preference<Boolean> notificationEnabledPref;
-    /**
-     * {@link Preference}-reference for easier usage of the saved value for
-     * notificationShowOnLockScreen in the {@link RxSharedPreferences}.
-     */
-    private Preference<Boolean> notificationShowOnLockScreenPref;
-    /**
-     * {@link Preference}-reference for easier usage of the saved value for notificationPriority
-     * in the {@link RxSharedPreferences}.
-     */
-    private Preference<String> notificationPriorityPref;
     /**
      * {@link Preference}-reference for easier usage of the saved value for packageNames in the
      * {@link RxSharedPreferences}.
@@ -79,28 +49,11 @@ public class NotificationService extends Service {
 
         RxSharedPreferences rxSharedPreferences = RxSharedPreferences.create(PreferenceManager.getDefaultSharedPreferences(this));
 
-        int notificationIcon = USE_VECTOR_DRAWABLES ? NOTIFICATION_ICON_ID_VECTOR : NOTIFICATION_ICON_ID_NOT_VECTOR;
-
-        customNotificationHelper = new CustomNotificationHelper(this, notificationIcon);
+        customNotificationHelper = new CustomNotificationHelper(this);
 
         notificationEnabledPref = rxSharedPreferences.getBoolean(
                 getString(R.string.pref_notification_enabled),
                 Boolean.parseBoolean(getString(R.string.pref_notification_enabled_default_value))
-        );
-
-        transparentIconPref = rxSharedPreferences.getBoolean(
-                getString(R.string.pref_transparent_icon),
-                Boolean.parseBoolean(getString(R.string.pref_transparent_icon_default_value))
-        );
-
-        notificationShowOnLockScreenPref = rxSharedPreferences.getBoolean(
-                getString(R.string.pref_notification_show_on_lock_screen),
-                Boolean.parseBoolean(getString(R.string.pref_notification_show_on_lock_screen_default_value))
-        );
-
-        notificationPriorityPref = rxSharedPreferences.getString(
-                getString(R.string.pref_notification_priority),
-                getString(R.string.pref_notification_priority_option_default)
         );
 
         GsonPreferenceAdapter<List> adapter = new GsonPreferenceAdapter<>(new Gson(), List.class);
@@ -125,57 +78,18 @@ public class NotificationService extends Service {
      */
     private void initService() {
         if (customNotificationHelper == null ||
-                notificationShowOnLockScreenPref == null ||
-                notificationPriorityPref == null ||
                 packageNamesPref == null)
             onError();
 
         // subscribe to changes in notificationEnabledPref
-        notificationEnabledPref.asObservable().subscribe(notificationEnabled -> {
-            this.notificationEnabled = notificationEnabled;
+        notificationEnabledPref.asObservable().subscribe(enabled -> {
+            this.notificationEnabled = enabled;
 
-            if (notificationEnabled) {
+            if (enabled) {
                 ApplicationModel[] applicationModels = ApplicationModel.prepareApplicationModelsArray(this, packageNamesPref.get());
                 showNotification(applicationModels);
             } else
                 hideNotification();
-        });
-
-        // subscribe to changes in transparentIconPref
-        transparentIconPref.asObservable().subscribe(transparentIcon -> {
-            int notificationIcon;
-            if (transparentIcon)
-                notificationIcon = USE_VECTOR_DRAWABLES ? NOTIFICATION_ICON_ID_VECTOR_TRANSPARENT : NOTIFICATION_ICON_ID_NOT_VECTOR_TRANSPARENT;
-            else
-                notificationIcon = USE_VECTOR_DRAWABLES ? NOTIFICATION_ICON_ID_VECTOR : NOTIFICATION_ICON_ID_NOT_VECTOR;
-
-            customNotificationHelper.setNotificationIcon(notificationIcon);
-        });
-
-        // subscribe to changes in notificationShowOnLockScreenPref
-        notificationShowOnLockScreenPref.asObservable().subscribe(notificationShowOnLockScreen -> {
-            int notificationVisibility = NotificationCompat.VISIBILITY_PUBLIC;
-            if (!notificationShowOnLockScreen)
-                notificationVisibility = NotificationCompat.VISIBILITY_SECRET;
-
-            customNotificationHelper.setNotificationVisibility(notificationVisibility);
-        });
-
-        // subscribe to changes in notificationPriorityPref
-        notificationPriorityPref.asObservable().subscribe(notificationPriority -> {
-            if (TextUtils.isEmpty(notificationPriority)) return;
-
-            int notificationPriorityValue = Notification.PRIORITY_MAX;
-            if (notificationPriority.equals(getString(R.string.pref_notification_priority_value_high)))
-                notificationPriorityValue = Notification.PRIORITY_HIGH;
-            else if (notificationPriority.equals(getString(R.string.pref_notification_priority_value_default)))
-                notificationPriorityValue = Notification.PRIORITY_DEFAULT;
-            else if (notificationPriority.equals(getString(R.string.pref_notification_priority_value_low)))
-                notificationPriorityValue = Notification.PRIORITY_LOW;
-            else if (notificationPriority.equals(getString(R.string.pref_notification_priority_value_min)))
-                notificationPriorityValue = Notification.PRIORITY_MIN;
-
-            customNotificationHelper.setNotificationPriority(notificationPriorityValue);
         });
 
         // subscribe to changes in packageNamesPref
@@ -203,8 +117,7 @@ public class NotificationService extends Service {
     }
 
     /**
-     * Calls {@link CustomNotificationHelper} to show the notification with given array of
-     * {@link ApplicationModel}s.
+     * Calls {@link CustomNotificationHelper} to show the notification with given array of {@link ApplicationModel}s.
      *
      * @param applicationModels array of {@link ApplicationModel}s to show in notification
      */
