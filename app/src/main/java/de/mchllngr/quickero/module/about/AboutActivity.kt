720d034d5b8ab724779351ecfe71980deaf88d9f
Michael Langer <mail@michaellanger.me>
2020-10-31 11:00:25 +0100

    added links to about page

--- app/src/main/java/de/mchllngr/quickero/module/about/AboutActivity.kt
+++ app/src/main/java/de/mchllngr/quickero/module/about/AboutActivity.kt
@@ -3,6 +3,7 @@ package de.mchllngr.quickero.module.about
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.commit
@@ -22,13 +23,40 @@ class AboutActivity : AppCompatActivity() {
             toolbar.setNavigationOnClickListener { onBackPressed() }
         }
 
+        val description = buildString {
+            append("<b>")
+            appendUrl(R.string.url_github, R.string.github)
+            appendSeparator()
+            appendUrl(R.string.url_play_store, R.string.play_store)
+            appendSeparator()
+            appendUrl(R.string.url_privacy_policy, R.string.privacy_policy)
+            append("</b>")
+        }
+
         supportFragmentManager.commit {
             replace(
                 R.id.fragment_container_view,
                 LibsBuilder()
+                    .withFields(R.string::class.java.fields)
+                    .withAboutDescription(description)
                     .supportFragment()
             )
         }
+    }
+
+    private fun StringBuilder.appendUrl(
+        @StringRes url: Int,
+        @StringRes text: Int
+    ) {
+        append("<a href=\"")
+        append(getString(url))
+        append("\">")
+        append(getString(text))
+        append("</a>")
+    }
+
+    private fun StringBuilder.appendSeparator() {
+        append("&nbsp;&nbsp;|&nbsp;&nbsp;")
     }
 
     companion object {
