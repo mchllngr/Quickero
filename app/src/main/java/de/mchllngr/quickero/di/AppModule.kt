98d838a610b3bfc4ea9c7144ff3fae7ee0ce0f5f
Michael Langer <mail@michaellanger.me>
2020-12-06 17:46:18 +0100

    fixed ApplicationComponent deprecation

--- app/src/main/java/de/mchllngr/quickero/di/AppModule.kt
+++ app/src/main/java/de/mchllngr/quickero/di/AppModule.kt
@@ -9,12 +9,12 @@ import androidx.datastore.preferences.createDataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     private const val DATA_STORE_NAME = "settings_pref"
